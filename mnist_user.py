bd9737656adb4b3e291333abce8b30875a36c0e0
Purinat33 <Purinat33@gmail.com>
2023-07-08 01:09:37 +0700

    Upload your own handwritten image file now working as intended

--- mnist_user.py
+++ mnist_user.py
@@ -186,14 +186,76 @@ def main():
 
     st.divider()
     st.header("Upload your own image!")
+    processed_imaged_url = (
+        "https://github.com/Purinat33/Streamlit-MNIST/raw/master/image_process.png"
+    )
+    example_image = load_img(processed_imaged_url, "image_process.png")
+    st.write("Example of what will happen to your uploaded file.")
+    st.image(example_image)
 
     # A part for the user to upload their own handwriting image
     img_upload = st.file_uploader(
         "Upload your own handwriting image", type=["jpg", "png"]
     )
     if img_upload is not None:
+        # Preprocessed the data like the example
         user_image = Image.open(img_upload)
-        st.image(user_image)
+        fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(10, 6))
+
+        # Hide the ticks
+        for axis in ax.flat:
+            axis.set_xticks([])
+            axis.set_yticks([])
+
+        # Original Image
+        ax[0, 0].set_title("Original Image")
+        ax[0, 0].imshow(user_image)
+
+        # Resized Image (28 * 28)
+        resized_image = user_image.resize((28, 28))
+        ax[0, 1].set_title("Resized Image")
+        ax[0, 1].imshow(resized_image)
+
+        # Grayscale Image
+        grayscaled_image = resized_image.convert("L")
+        ax[0, 2].set_title("Grayscaled Image")
+        ax[0, 2].imshow(grayscaled_image, cmap="gray")
+
+        # Invert the image (so the text is white, and background is black)
+        inverted_image = 255 - np.array(grayscaled_image)
+        ax[1, 0].set_title("Inverted Image")
+        ax[1, 0].imshow(inverted_image, cmap="gray")
+
+        # Normalize the image (divided by 255 to make them between 0 - 1)
+        normalized_image = inverted_image / 255.0
+        ax[1, 1].set_title("Normalized Image")
+        ax[1, 1].imshow(normalized_image, cmap="gray")
+
+        # Reshaped image
+        reshaped_image = normalized_image.reshape((28, 28))
+        ax[1, 2].set_title("Reshaped Image")
+        ax[1, 2].imshow(reshaped_image, cmap="gray")
+
+        st.pyplot(fig)
+
+        # The actual data we're going to use
+        reshaped_image = normalized_image.reshape((1, 28 * 28))
+
+        true_label = st.selectbox(
+            "What digit does your image represent?", [i for i in range(10)]
+        )
+        predicted_label = model.predict(reshaped_image)
+        predicted_label = predicted_label.argmax(axis=1)
+
+        st.subheader(f"True Value: {true_label}")
+        st.subheader(f"Predicted Value: {predicted_label.tolist()[0]}")
+
+        if true_label == predicted_label.tolist()[0]:
+            st.subheader(":green[CORRECT]")
+        else:
+            st.subheader(":red[INCORRECT]")
+
+        st.divider()
 
     st.header("Model Performance Overview")
     # st.subheader(f"Loss: {model.evaluate(X_test_re, Y_test_re)[0]}")
